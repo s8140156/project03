00b099c34d428604af11a9d5ff4e99ca9e2e1a80
s8140156 <s8140156@gmail.com>
2024-04-30 00:29:53 +0800

    初始化作品集3

---
+++ api/db.php
@@ -0,0 +1,125 @@
+<?php
+
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB{
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db68";
+    protected $pdo;
+    protected $table;
+
+    function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+    function all($where='',$other=''){
+        $sql="select * from `$this->table` ";
+        $sql=$this->sql_all($sql,$where,$other);
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    function count($where='',$other=''){
+        $sql="select count(*) from `$this->table` ";
+        $sql=$this->sql_all($sql,$where,$other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    private function math($math,$col,$array='',$other=''){
+        $sql="select $math(`$col`) from `$this->table` ";
+        $sql=$this->sql_all($sql,$array,$other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function sum($col,$where='',$other=''){
+        return $this->math('sum',$col,$where,$other);
+    }
+    function max($col,$where='',$other=''){
+        return $this->math('max',$col,$where,$other);
+    }
+    function min($col,$where='',$other=''){
+        return $this->math('min',$col,$where,$other);
+    }
+    function find($id){
+        $sql="select * from `$this->table` ";
+        if(is_array($id)){
+            $tmp=$this->a2s($id);
+            $sql .=" where ".join(" && ",$tmp);
+        }else if(is_numeric($id)){
+            $sql .=" where `id`='$id'";
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    function save($array){
+        if(isset($array['id'])){
+            $sql="update `$this->table` set ";
+            if(!empty($array)){
+                $tmp=$this->a2s($array);
+            }
+            $sql .=join(",",$tmp);
+            $sql .=" where `id`='{$array['id']}'";
+        }else{
+            $sql="insert into `$this->table` ";
+            $cols="(`" .join("`,`",array_keys($array)) . "`)";
+            $vals="('" .join("','",$array) . "')";
+            $sql=$sql . $cols . " values " . $vals;
+        }
+        return $this->pdo->exec($sql);
+    }
+    function del($id){
+        $sql="delete from `$this->table` ";
+        if(is_array($id)){
+            $tmp=$this->a2s($id);
+            $sql .=" where ".join(" && ",$tmp);
+        }else if(is_numeric($id)){
+            $sql .=" where `id`='$id'";
+        }
+        return $this->pdo->exec($sql);
+    }
+    function q($sql){
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    private function a2s($array){
+        foreach($array as $col=>$value){
+            $tmp[]="`$col`='$value'";
+        }
+        return $tmp;
+    }
+    private function sql_all($sql,$array,$other){
+        if(isset($this->table) && !empty($this->table)){
+            if(is_array($array)){
+                if(!empty($array)){
+                    $tmp=$this->a2s($array);
+                }
+                $sql .=" where ".join(" && ",$tmp);
+            }else{
+                $sql .=" $array";
+            }
+            $sql .=$other;
+            return $sql;
+        }
+    }
+}
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function to($url){
+    header("location:$url");
+}
+
+//設定場次陣列
+$sess=[1=>'14:00~16:00',
+       2=>'16:00~18:00',
+       3=>'18:00~20:00',
+       4=>'20:00~22:00',
+       5=>'22:00~24:00',
+      ];
+
+$Poster=new DB('poster');
+$Movie=new DB('movie');
+$Order=new DB('orders');
+
+
+
+
+?>
